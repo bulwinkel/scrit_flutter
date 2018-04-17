1ed1583d83d12270e067c4421cf3c4996932648a
Bulwinkel <kelvin@bulwinkel.com>
2018-04-16 20:16:41 -0500

    Setup basic list

--- lib/ui/task_list.dart
+++ lib/ui/task_list.dart
@@ -3,29 +3,39 @@ import 'package:flutter/material.dart';
 class TaskListWidget extends StatefulWidget {
   TaskListWidget({Key key}) : super(key: key);
 
-  @override createState() => _TaskListWidgetState();
+  @override createState() => new _TaskListWidgetState();
 }
 
 class _TaskListWidgetState extends State<TaskListWidget> {
 
+  final todos = new List<String>.generate(5, (int index) => "Tests");
 
-  void _incrementCounter() {
+  Iterable<ListTile> _generateListTiles() {
+    return todos.map((todo) => new ListTile(
+      title: new Text(todo),
+    ));
+  }
+
+  void _addTodo() {
     setState(() {
-
+      todos.add("A new todo");
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Scrit"),
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Scrit"),
       ),
-//      body:
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+      body: new ListView(
+        padding: new EdgeInsets.symmetric(vertical: 8.0),
+        children: _generateListTiles().toList(),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _addTodo,
         tooltip: 'Increment',
-        child: Icon(Icons.add),
+        child: new Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
